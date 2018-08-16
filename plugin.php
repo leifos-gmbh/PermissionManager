8b74d2c2d5f1d55dd8c26dc504794b816b8b4214
smeyer-ilias <smeyer.ilias@gmx.de>
2018-08-16 11:35:50 +0200

    Increased version

--- plugin.php
+++ plugin.php
@@ -4,12 +4,12 @@
 $id = "lfpm";
  
 // code version; must be changed for all code changes
-$version = "0.0.1";
+$version = "5.3.7.1";
  
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
-$ilias_min_version = "5.1.4";
-$ilias_max_version = "5.2.999";
+$ilias_min_version = "5.3.7";
+$ilias_max_version = "5.3.999";
  
 // optional, but useful: Add one or more responsible persons and a contact email
 $responsible = "Stefan Meyer";
